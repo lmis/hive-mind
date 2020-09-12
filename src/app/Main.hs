e6df6e561e85bb7d56be6f8f5fc6011ff97e6eb7
Lionel Miserez <lionel.miserez@gmail.com>
2020-09-12 12:08:19 +0200

    Add basic game state

--- src/app/Main.hs
+++ src/app/Main.hs
@@ -4,6 +4,10 @@ module Main
   )
 where
 
+import           Data.Map.Strict                ( Map
+                                                , (!?)
+                                                )
+import qualified Data.Map.Strict               as Map
 import           Control.Monad.IO.Class         ( liftIO )
 import           Control.Monad                  ( forever
                                                 , void
@@ -36,11 +40,19 @@ import           Brick.BChan                    ( BChan
                                                 , newBChan
                                                 , writeBChan
                                                 )
-import           Lens.Micro                     ( (&), (^.), (%~) )
+import           Lens.Micro                     ( (&)
+                                                , (^.)
+                                                , (%~)
+                                                )
 import           Lens.Micro.Platform            ( makeLenses )
 
 data AppEvent = AdvanceGame deriving (Eq, Show, Ord)
-type GameState = ()
+data GameObject = Hiveling | Nutrition | HiveEntrance | Pheromone | Obstacle deriving (Eq, Show, Ord)
+type Position = (Int, Int)
+newtype GameState = GameState {
+  _objects :: Map Position GameObject
+} deriving (Eq, Show)
+makeLenses ''GameState
 type Name = ()
 data AppState = AppState {
   _gameState :: !GameState,
@@ -48,6 +60,16 @@ data AppState = AppState {
 } deriving (Eq, Show)
 
 makeLenses ''AppState
+
+startingState :: GameState
+startingState = GameState { _objects = Map.fromList $ entrance : boundary ++ hivelings ++ nutrition }
+ where
+  entrance  = ((0, 0), HiveEntrance)
+  hivelings = [ ((0, y), Hiveling) | y <- [1 .. 4] ]
+  boundary =
+    [ ((x, y), Obstacle) | x <- [-10 .. 10], y <- [-10, 10] ]
+      ++ [ ((x, y), Obstacle) | x <- [-10, 10], y <- [-10 .. 10] ]
+  nutrition = [ ((9, y), Nutrition) | y <- [-10 .. 0] ]
 
 app :: App AppState AppEvent Name
 app = App { appDraw         = drawUI
@@ -60,11 +82,11 @@ app = App { appDraw         = drawUI
 main :: IO ()
 main = do
   -- channel to inject events into main loop
-  chan         <- newBChan 10
-  _            <- advanceGame chan
+  chan       <- newBChan 10
+  _          <- advanceGame chan
 
-  initialVty   <- buildVty
-  void $ customMain initialVty buildVty (Just chan) app $ AppState { _gameState = (), _iteration=0, .. }
+  initialVty <- buildVty
+  void $ customMain initialVty buildVty (Just chan) app $ AppState { _gameState = startingState, _iteration = 0, .. }
  where
   buildVty = do
     vty <- V.mkVty V.defaultConfig
@@ -80,14 +102,23 @@ advanceGame chan = forkIO $ forever $ do
 handleEvent :: AppState -> BrickEvent Name AppEvent -> EventM Name (Next AppState)
 handleEvent s (VtyEvent (V.EvKey (V.KChar 'c') [V.MCtrl])) = halt s
 handleEvent s (VtyEvent (V.EvKey (V.KChar 'd') [V.MCtrl])) = halt s
-handleEvent s (AppEvent AdvanceGame) = continue $ s & iteration %~ (+1)
+handleEvent s (AppEvent AdvanceGame) = continue $ s & iteration %~ (+ 1)
 handleEvent s _ = continue s
 
+drawGameState :: GameState -> [Widget Name]
+drawGameState g = [str $ unlines [ [ renderPosition (x, y) | x <- [-10 .. 10] ] | y <- [-10 .. 10] ]]
+ where
+  renderPosition :: Position -> Char
+  renderPosition (x, y) = maybe ' ' symbol ((g ^. objects) !? (x, y))
+  symbol :: GameObject -> Char
+  symbol Hiveling     = 'H'
+  symbol Nutrition    = 'N'
+  symbol HiveEntrance = 'E'
+  symbol Pheromone    = 'P'
+  symbol Obstacle     = 'X'
+
 drawUI :: AppState -> [Widget Name]
-drawUI s =
-  [ C.vCenter $ vBox
-      (C.hCenter <$> [labeledVBox "Hive Mind" $ C.hCenter <$> [str $ "Hello: " ++ show (s ^. gameState) ++ show (s ^. iteration)]])
-  ]
+drawUI s = [C.vCenter $ vBox (C.hCenter <$> [labeledVBox "Hive Mind" $ C.hCenter <$> drawGameState (s ^. gameState)])]
 
 theMap :: AttrMap
 theMap = attrMap V.defAttr []
